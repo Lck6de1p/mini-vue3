cfc4df9115abbd63cd822c936e4e79280503f825
linchuanke <310504719@qq.com>
2022-05-28 10:10:40 +0800

    readonly逻辑+单测

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,17 +1,15 @@
-import { track, trigger } from './effect'
+import { mutableHandlers, readonlyHandlers } from './baseHandlers';
+import { track, trigger } from './effect';
+
+
 export function reactive(raw) {
-  return new Proxy(raw, {
-    get(target, key) {
-      // {foo: 1};
-      // foo
-      const res = Reflect.get(target, key);
-      track(target, key);
-      return res;
-    },
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value);
-      trigger(target, key);
-      return res;
-    }
-  });
+  return createActiveObject(raw, mutableHandlers);
+}
+
+export function readonly(raw) {
+  return createActiveObject(raw, readonlyHandlers);
+}
+
+function createActiveObject(raw: any, baseHandlers) {
+  return new Proxy(raw, baseHandlers)
 }
