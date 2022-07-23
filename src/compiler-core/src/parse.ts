18619d19247957e069a6ec9280395c8cffde2fbf
linchuanke <310504719@qq.com>
2022-07-23 15:01:12 +0800

    feat: 解析element

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,7 +1,9 @@
 import { NodeTypes } from "./ast";
 
-const openDelimiter = "{{";
-const closeDelimiter = "}}";
+const enum TagType {
+  START,
+  END
+}
 
 export function baseParse(content: string) {
   const context = createParseContext(content);
@@ -17,14 +19,42 @@ function createRoot(children) {
 function parseChildren(context) {
   const nodes: any = [];
   let node;
-  if (context.source.startsWith(openDelimiter)) {
+  const s = context.source;
+  if (s.startsWith("{{")) {
     node = parseInterpolation(context);
+  } else if (s[0] === ("<")) {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context);
+    }
   }
   nodes.push(node);
   return nodes
 }
 
+function parseElement(context) {
+  // 1. 解析tag
+  // 2. 删除处理完成的代码
+  const element = parseTag(context, TagType.START);
+  parseTag(context, TagType.END);
+  return element;
+}
+
+function parseTag(context, type: TagType) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  const tag = match[1];
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1);
+
+  if (type === TagType.END) return;
+  return {
+    type: NodeTypes.ELEMENT,
+    tag
+  }
+}
+
 function parseInterpolation(context) {
+  const openDelimiter = "{{";
+  const closeDelimiter = "}}";
   const closedIndex = context.source.indexOf(closeDelimiter, openDelimiter.length);
 
   advanceBy(context, openDelimiter.length);
